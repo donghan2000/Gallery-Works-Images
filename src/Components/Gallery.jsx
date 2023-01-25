12d0d905cacb72b471d7b80ce7f8b92cbebff855
donghan2000 <donghan4ever@gmail.com>
2023-01-25 23:58:06 +0800

    added on click division

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,5 +1,5 @@
 import React , {useState , useEffect} from 'react'
-import { a } from '@react-spring/web'
+import { a, useTransition, useSpring, useChain, config,  animated, useSpringRef } from '@react-spring/web'
 import { Slider } from './Slider'
 import { items } from './items'
 
@@ -10,20 +10,56 @@ export default function Gallery(){
   const [activeIndex, setActiveIndex] = useState(0);
   const [isHovering, setIsHovering] = useState(false);
   const [activeHovered , setHovered] = useState(0)
+  const [open, set] = useState(false);
 
   const onEnter = (i) => {
     setIsHovering(true);
     setHovered(i);
   }
-
   
   const onLeave = (event)=>{
     setIsHovering(false)
   }
 
+  const openModal = (i) =>{
+    console.log("Index clicked is " + i )
+    set((open) => !open)
+  }
+
+  const springApi = useSpringRef();
+  const { size, ...rest } = useSpring({
+    ref: springApi,
+    config: config.stiff,
+    from: { height: "0px", width:"0px", background: "hotpink" , opacity: "0" },
+    to: {
+      width: open ? "501px" : "0px",
+      height: open ? "262px" : "0px",
+      background: open ? "white" : "hotpink",
+      opacity: open ? "1" : "0"
+    }
+  });
+
+  const transApi = useSpringRef();
+  const transition = useTransition(open ? items : [], {
+    ref: transApi,
+    trail: 400 / items.length,
+    from: { opacity: 0, scale: 0 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0 }
+  });
+
+  // This will orchestrate the two animations above, comment the last arg and it creates a sequence
+  useChain(open ? [springApi, transApi] : [transApi, springApi], [
+    0,
+    open ? 0.1 : 0.6
+  ]);
+
   return <>
 
     <div className="flex fill center container">
+      <div className='section-titles'>
+        <h1>My Works</h1>
+      </div>
       <div className="main">
         <Slider items={items} width={700} visible={3} setActiveIndex={setActiveIndex}>
           {({ imageUrl , Title }, i) => (
@@ -43,11 +79,25 @@ export default function Gallery(){
                 onPointerLeave={(onLeave)} 
               ></div> */}
 
+
                <div className='circle'
-                style={{  opacity: i === activeHovered && isHovering ? "1" : "0"  }}
+                  style={{  opacity: i === activeHovered && isHovering ? "1" : "0"  }}
+                  onPointerEnter={() => onEnter(i)}
+                  onPointerLeave={(onLeave)} 
+                  onClick={() => openModal(i)}
+               ></div>
+
+               
+              <animated.div
+                style={{ ...rest}}
+                className="pinkDiv"
+                onClick={() => set((open) => !open)}
                 onPointerEnter={() => onEnter(i)}
                 onPointerLeave={(onLeave)} 
-               ></div>
+              >
+              </animated.div>
+
+
 
             </div>
           )}
