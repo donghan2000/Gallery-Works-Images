8d781257b4ddd2cd4034baac80a4932b63c0f764
donghan2000 <donghan4ever@gmail.com>
2023-01-26 17:20:20 +0800

    Changed some codes on the modal div  with responsive in mind

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,41 +1,43 @@
-import React , {useState , useEffect} from 'react'
-import { a, useTransition, useSpring, useChain, config,  animated, useSpringRef } from '@react-spring/web'
+import React, { useState, useEffect } from 'react'
+import { a, useTransition, useSpring, useChain, config, animated, useSpringRef } from '@react-spring/web'
 import { Slider } from './Slider'
 import { items } from './items'
 
 import styles from './styles.module.css'
 
-export default function Gallery(){
+export default function Gallery() {
 
   const [activeIndex, setActiveIndex] = useState(0);
   const [isHovering, setIsHovering] = useState(false);
-  const [activeHovered , setHovered] = useState(0)
+  const [activeHovered, setHovered] = useState(0)
   const [open, set] = useState(false);
+  const [activeOpen , setOpen] = useState(0)
 
   const onEnter = (i) => {
     setIsHovering(true);
     setHovered(i);
   }
-  
-  const onLeave = (event)=>{
+
+  const onLeave = (event) => {
     setIsHovering(false)
   }
 
-  const openModal = (i) =>{
-    console.log("Index clicked is " + i )
+  const openModal = (i) => {
+    console.log("Index clicked is " + i)
     set((open) => !open)
+    setOpen(i)
+
   }
 
   const springApi = useSpringRef();
   const { size, ...rest } = useSpring({
     ref: springApi,
     config: config.stiff,
-    from: { height: "0px", width:"0px", background: "hotpink" , opacity: "0" },
+    from: { size: "0%", background: "hotpink", opacity: "0" },
     to: {
-      width: open ? "501px" : "0px",
-      height: open ? "262px" : "0px",
       background: open ? "white" : "hotpink",
-      opacity: open ? "1" : "0"
+      opacity: open ? "1" : "0",
+      size: open ? "100%" : "0%",
     }
   });
 
@@ -62,57 +64,51 @@ export default function Gallery(){
       </div>
       <div className="main">
         <Slider items={items} width={700} visible={3} setActiveIndex={setActiveIndex}>
-          {({ imageUrl , Title }, i) => (
+          {({ imageUrl, Title }, i) => (
             <div className="content">
-              <div className="marker">{String(i+ 1).padStart(2, '0')}. {Title}</div>
-              
-              <a.div 
-                className="image" 
-                style={{ backgroundImage: imageUrl , opacity: i === activeHovered && isHovering ? "0.25" : "1"  } }
-                onPointerEnter={() => onEnter(i)}  
-                onPointerLeave={(onLeave)} 
+              <div className="marker">{String(i + 1).padStart(2, '0')}. {Title}</div>
+
+              <a.div
+                className="image"
+                style={{ backgroundImage: imageUrl, opacity: i === activeHovered && isHovering ? "0.25" : "1" }}
+                onPointerEnter={() => onEnter(i)}
+                onPointerLeave={(onLeave)}
               />
 
-              {/* <div className='middle' 
-                style={{  display: i === activeHovered && isHovering ? "block" : "none" , opacity: i === activeHovered && isHovering ? "0.8" : "0"  }}
+              <div className='circle'
+                style={{ opacity: i === activeHovered && isHovering ? "1" : "0" }}
                 onPointerEnter={() => onEnter(i)}
-                onPointerLeave={(onLeave)} 
-              ></div> */}
+                onPointerLeave={(onLeave)}
+                onClick={() => openModal(i)}
+              ></div>
 
-
-               <div className='circle'
-                  style={{  opacity: i === activeHovered && isHovering ? "1" : "0"  }}
-                  onPointerEnter={() => onEnter(i)}
-                  onPointerLeave={(onLeave)} 
-                  onClick={() => openModal(i)}
-               ></div>
-
-               
-              <animated.div
-                style={{ ...rest}}
-                className="pinkDiv"
-                onClick={() => set((open) => !open)}
-                onPointerEnter={() => onEnter(i)}
-                onPointerLeave={(onLeave)} 
+              <div className='imageWrapper'
+                style={{ zIndex: i === activeOpen &&  open? "1" : "-1",  opacity: i === activeOpen &&  open ? "1" : "0" }}
               >
-              </animated.div>
-
-
-
+                <animated.div
+                  style={{ ...rest, width: size, height: size }}
+                  className="modal"
+                  onClick={() => set((open) => !open)}
+                  onPointerEnter={() => onEnter(i)}
+                  onPointerLeave={(onLeave)}
+                >
+                </animated.div>
+              </div>
             </div>
           )}
         </Slider>
       </div>
+
       <div className="navbar">
         {items.map((item, i) => (
-          <div 
+          <div
             key={i}
             className={`dot ${i === activeIndex ? styles.active : ' '}`}
           />
         ))}
-        </div>
-     </div>
+      </div>
+    </div>
 
-    </>
-    
+  </>
+
 }
